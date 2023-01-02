b51ea625220da267fe4dd3609d5427607f10b206
Utkarsh Dixit <98089242+Utkarshh-Dixit@users.noreply.github.com>
2023-01-03 02:30:42 +0530

    Prefix evaluation and conversion

---
+++ Stack and Queues/prefixevalandconv.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class prefixevalandconv {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+
+        Stack<Integer> vs = new Stack<>();
+        Stack<String> is = new Stack<>();
+        Stack<String> ps = new Stack<>();
+
+        for (int i = exp.length() - 1; i >= 0; i--) {
+            char ch = exp.charAt(i);
+
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                int v1 = vs.pop();
+                int v2 = vs.pop();
+                int v = operation(v1, v2, ch);
+                vs.push(v);
+
+                String iv1 = is.pop();
+                String iv2 = is.pop();
+                String iv = "(" + iv1 + ch + iv2 + ")";
+                is.push(iv);
+
+                String pv1 = ps.pop();
+                String pv2 = ps.pop();
+                String pv = pv1 + pv2 + ch;
+                ps.push(pv);
+            } else {
+                vs.push(ch - '0');
+                is.push(ch + "");
+                ps.push(ch + "");
+            }
+        }
+
+        System.out.println(vs.pop());
+        System.out.println(is.pop());
+        System.out.println(ps.pop());
+    }
+
+    public static int operation(int v1, int v2, char ch) {
+        if (ch == '+') {
+            return v1 + v2;
+        } else if (ch == '-') {
+            return v1 - v2;
+        } else if (ch == '*') {
+            return v1 * v2;
+        } else {
+            return v1 / v2;
+        }
+    }
+}
